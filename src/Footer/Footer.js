a60ed389cf1d3fce16fead982b1abdbe4f8e3f78
Gayatri Vaka <vakagayatrireddy@gmail.com>
2020-10-14 03:11:15 -0400

    Brought personal-budget code in

---
+++ src/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Footer() {
+  return (
+    <footer>
+    <div className="center">
+      All rights reserved &copy; Gayatri Vaka
+    </div>
+</footer>
+  );
+}
+
+export default Footer;
